6ea297c6991f1999a7af8ce4ac887a111466b439
1103-11 <1103-11@1103-11-PC>
2018-03-13 15:43:31 +0900

    인증 추가 완료

---
+++ src/total/controller/TestController.java
@@ -0,0 +1,20 @@
+package total.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import total.service.MailService;
+
+@Controller
+public class TestController {
+	@Autowired
+	MailService mailService;
+	
+	@RequestMapping("/test/email")
+	public void emailTestHandle(@RequestParam String target) {
+		boolean b = mailService.sendWelcomeMail(target);
+		System.out.println("메일 전송 결과 = " + b);
+	}
+}
